d309a4f8fb847516529366e3c85ef160588beb9b
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-05 18:31:58 +0200

    Replace mocha by AVA

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-'use strict';
-var assert = require('assert');
-var osxVersion = require('./');
+import test from 'ava';
+import fn from './';
 
-it('should return the current OS X version', function (cb) {
-	osxVersion(function (err, version) {
+test(t => {
+	t.plan(2);
+
+	fn((err, version) => {
 		console.log('Version:', version);
-		assert(!err, err);
-		assert(version.length > 0);
-		cb();
+		t.ifError(err);
+		t.true(version.length > 0);
 	});
 });
