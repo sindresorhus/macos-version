464f0e2eae94be637becfce2f29deb0dd00f8dd7
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-19 18:09:16 +0200

    [BREAKING] ES2015ify - Requires Node.js >=4

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 'use strict';
-var execFile = require('child_process').execFile;
-var pify = require('pify');
-var Promise = require('pinkie-promise');
+const execa = require('execa');
 
-module.exports = function () {
-	return pify(execFile, Promise)('sw_vers', ['-productVersion']).then(function (stdout) {
-		return stdout.trim();
-	});
-};
+module.exports = () => execa.stdout('sw_vers', ['-productVersion']);
